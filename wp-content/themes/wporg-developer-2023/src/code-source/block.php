eab8aadcbeaa0607849367548db94879a4128590
Carsten Bach <mail@carsten-bach.de>
2024-08-20 11:12:10 +0200

    Fix GitHub vs Trac links

--- wp-content/themes/wporg-developer-2023/src/code-source/block.php
+++ wp-content/themes/wporg-developer-2023/src/code-source/block.php
@@ -103,9 +103,11 @@ function get_source_content( $post_id ) {
 				)
 			);
 
-			$output .= sprintf( '<p class="wporg-dot-link-list">%s</p>', implode( ' ', array( $view_reference_button, $view_trac_button, $view_github_button ) ) );
+			// $output .= sprintf( '<p class="wporg-dot-link-list">%s</p>', implode( ' ', array( $view_reference_button, $view_trac_button, $view_github_button ) ) );
+			$output .= sprintf( '<p class="wporg-dot-link-list">%s</p>', implode( ' ', array( $view_reference_button, $view_github_button ) ) );
 		} else {
-			$output .= sprintf( '<p class="wporg-dot-link-list">%s</p>', implode( ' ', array( $view_reference_button, $view_trac_button ) ) );
+			// $output .= sprintf( '<p class="wporg-dot-link-list">%s</p>', implode( ' ', array( $view_reference_button, $view_trac_button ) ) );
+			$output .= sprintf( '<p class="wporg-dot-link-list">%s</p>', implode( ' ', array( $view_reference_button ) ) );
 		}
 	}
 
